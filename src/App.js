fe63e944e4aaeac78b729d9e88b76cd5864f3979
izcoser <76838137+izcoser@users.noreply.github.com>
2022-05-15 23:18:20 -0300

    Add Github link
    Font awesome is set up. No idea what to do with this site.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEthereum } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
+
+//<FontAwesomeIcon icon={faEthereum} />
+//<a href="https://twitter.com/matedegaveta" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
 
 function App() {
   return (
@@ -8,7 +14,13 @@ function App() {
         <p className="App-message">
           in space, no one can hear you scream
         </p>
+        
+        <div className="links">
+          
+          <a href="https://github.com/izcoser" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+        </div>
       </header>
+      
     </div>
   );
 }
